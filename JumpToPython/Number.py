dc4185d03af4cb9e29705e9d371668eabeb0348e
seonghyeon <tjdgusdkf@gmail.com>
2020-09-22 01:01:43 +0900

    - READMD.md 개선 - 파이썬 자료형 공부     2. 문자열(String)

--- JumpToPython/Number.py
+++ JumpToPython/Number.py
@@ -1,36 +1,35 @@
-# Integer(정수)
+# 정수(Integer)
 a = 123
 a = -178
 a = 0
 
-# Floating-point(실수, 소수점이 포함된 숫자)
+# 살수(Floating-point, 소수점이 포함된 숫자)
 a = 1.2
 a = -3.45
-# 컴퓨터식 지수 표현 방식
+## 컴퓨터식 지수 표현 방식
 a = 4.24E10
 a = 4.24e10
 a = 4.24e-10
 
 # 8진수와 16진수
-# 8진수(Octal)
+## 8진수(Octal)
 a = 0o177
 a = 0O177
-# 16진수(Hexadecimal)
+## 16진수(Hexadecimal)
 a = 0x8ff
 b = 0x8ABC
 
 # 사칙연산
 a = 3
 b = 4
-print(a + b)
-print(a - b)
-print(a * b)
-print(a / b)
-# **연산자(x의 y제곱)
-print(a ** b)
-# 나머지 연산자
-print (7 % 3)
-print (3 % 7)
-# 몫 연산자
-print (7 // 4)
-
+a + b
+a - b
+a * b
+a / b
+## **연산자(x의 y제곱)
+a ** b
+## 나머지 연산자
+7 % 3
+3 % 7
+## 몫 연산자
+7 // 4
